b4f7dc682cd04cafa2c40985affea7cc651d955e
Brian Sjoberg <brian_sjoberg@yahoo.com>
2025-08-28 05:45:50 -0400

    clean: even more linting.

--- src/Chapter03/threenames.py
+++ src/Chapter03/threenames.py
@@ -2,4 +2,3 @@ a = 'PC'                       # Define three attributes
 b = 'Phone'                    # Exported to other files
 c = 'Tablet'
 print(a, b, c)                 # Also used in this file
-
